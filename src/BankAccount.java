180bc950d95c20d969d3ec27ab7cd7e204d0a4f0
Hiago 7k <hiago7k@gmail.com>
2026-07-16 17:28:46 -0300

    metodo para retornar saldo

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,23 +1,28 @@
 public class BankAccount {
     private double saldo = 5000;
 
-    public void Sacar(double valor){
-        if(valor <= saldo) {
-            saldo =+  - valor;
-            System.out.println("Valor de " + valor + " sacado com sucesso" );
+    public void Sacar(double valor) {
+        if (valor <= saldo) {
+            saldo = +-valor;
+            System.out.println("Valor de " + valor + " sacado com sucesso");
             System.out.println("Seu saldo atual é de " + saldo);
-        }else {
+        } else {
             System.out.println("Saldo insuficiente");
         }
     }
 
 
-    public void Depositar(double valor){
-        if (valor > 0){
-            saldo =+ + valor;
+    public void Depositar(double valor) {
+        if (valor > 0) {
+            saldo = + +valor;
             System.out.println("Deposito de " + valor + " realizado");
             System.out.println("Saldo atual " + saldo);
-        }else {
-            System.out.println("Valor invg
+        } else {
+            System.out.println("Valor invalido para depositar");
+        }
+    }
+
+    public double consultarSaldo(){
+        return  saldo;
     }
 }
