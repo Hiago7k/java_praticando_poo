65577bf80755542ffc57f4964e9267162a24c440
Hiago 7k <hiago7k@gmail.com>
2026-07-16 17:24:19 -0300

    regra de negocio

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,15 @@
 public class BankAccount {
     private double saldo;
+
+    public void Sacar(double valor){
+
+        if(valor <= saldo) {
+            saldo = +saldo - valor;
+            System.out.println("Valor de " + valor + " sacado com sucesso" );
+            System.out.println("Seu saldo atual é de " + saldo);
+        }else {
+            System.out.println("Saldo insuficiente");
+        }
+
+    }
 }
