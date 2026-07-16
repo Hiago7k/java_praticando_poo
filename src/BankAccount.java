28390d01b2156f2d91e23b7aefead19593f5549e
Hiago 7k <hiago7k@gmail.com>
2026-07-16 17:26:35 -0300

    metodo de deposito

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,15 +1,23 @@
 public class BankAccount {
-    private double saldo;
+    private double saldo = 5000;
 
     public void Sacar(double valor){
-
         if(valor <= saldo) {
-            saldo = +saldo - valor;
+            saldo =+  - valor;
             System.out.println("Valor de " + valor + " sacado com sucesso" );
             System.out.println("Seu saldo atual é de " + saldo);
         }else {
             System.out.println("Saldo insuficiente");
         }
+    }
 
+
+    public void Depositar(double valor){
+        if (valor > 0){
+            saldo =+ + valor;
+            System.out.println("Deposito de " + valor + " realizado");
+            System.out.println("Saldo atual " + saldo);
+        }else {
+            System.out.println("Valor invg
     }
 }
