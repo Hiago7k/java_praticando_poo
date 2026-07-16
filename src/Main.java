5bb0c9b8b2735005d8aa396e72e67d05f497f358
Hiago 7k <hiago7k@gmail.com>
2026-07-16 17:16:38 -0300

    estanciando os animais

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 class Main{
     static void main() {
+        
+        Cachorro bob = new Cachorro();
+        bob.EmitirSom();
 
-
+        Gato beraldo = new Gato();
+        beraldo.EmitirSom();
     }
 }
 
