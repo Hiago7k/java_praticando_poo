e6307b69ac1568fa1c93fa31beac9664c2a9314a
Hiago 7k <hiago7k@gmail.com>
2026-07-16 16:26:26 -0300

    criando carros

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,20 @@ class Main{
     static void main() {
 
      Carro carro1 = new Carro();
-        
+        carro1.setModelo("Lancer");
+        carro1.setAno(2012);
+        carro1.exibeCarros();
+
+        Carro carro2 = new Carro();
+        carro2.setModelo("Mazda");
+        carro2.setAno(2011);
+        carro2.exibeCarros();
+
+        Carro carro3 = new Carro();
+        carro3.setModelo("Silva");
+        carro3.setAno(2010);
+        carro3.exibeCarros();
+
     }
 }
 
