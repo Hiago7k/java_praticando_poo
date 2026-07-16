5e63a3178ac37db4cbc1aea9631afca39c7b63ca
Hiago 7k <hiago7k@gmail.com>
2026-07-16 17:22:06 -0300

    account bank

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 class Main{
     static void main() {
 
-        Cachorro bob = new Cachorro();
-        bob.EmitirSom();
-        bob.abanarRabo();
 
-        Gato beraldo = new Gato();
-        beraldo.EmitirSom();
-        beraldo.arranharMoveis();
     }
 }
 
