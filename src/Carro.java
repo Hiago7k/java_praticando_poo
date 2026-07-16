a3244201a086b73b6dbf5c07af9481ffdccdd479
Hiago 7k <hiago7k@gmail.com>
2026-07-16 16:52:03 -0300

    ajustando logica

--- src/Carro.java
+++ src/Carro.java
@@ -5,35 +5,13 @@ public class Carro {
     private double precoAno2;
     private double precoAno3;
 
-    public double getPrecoAno1() {
-        return precoAno1;
-    }
-
-    public void setPrecoAno1(double precoAno1) {
-        this.precoAno1 = precoAno1;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
+    public void definirModelo(String modelo){
         this.modelo = modelo;
     }
 
-    public double getPrecoAno2() {
-        return precoAno2;
-    }
-
-    public void setPrecoAno2(double precoAno2) {
+    public void DefinirPreco(double precoAno1, double precoAno2, double precoAno3){
+        this.precoAno1 = precoAno1;
         this.precoAno2 = precoAno2;
-    }
-
-    public double getPrecoAno3() {
-        return precoAno3;
-    }
-
-    public void setPrecoAno3(double precoAno3) {
         this.precoAno3 = precoAno3;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 class Main{
     static void main() {
 
-    Carro carro1 = new Carro();
-    carro1.setModelo("Kwid");
-    carro1.setPrecoAno1(70000.0);
-    carro1.setPrecoAno2(60000.0);
-    carro1.setPrecoAno2(40000.0);
+
 
     }
 }
