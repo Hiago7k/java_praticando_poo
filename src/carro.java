689d269e71d9e283bcd8c3d0b7e20418e4f2d8b7
Hiago 7k <hiago7k@gmail.com>
2026-07-16 16:22:25 -0300

    criando super classe carro

---
+++ src/carro.java
@@ -0,0 +1,11 @@
+public class carro {
+
+    private String modelo;
+    private int ano;
+
+    
+    public void exibeCarros(){
+        System.out.println("Modelo:" + modelo);
+        System.out.println("Ano:" + ano);
+    }
+}
