9c75574ae51b605d4863998f1caae3af89aec0ab
Hiago 7k <hiago7k@gmail.com>
2026-07-20 16:43:37 -0300

    criando classes

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,6 @@
 public class Product {
+    private String nome = teclado;
+    private int quantidade = 23;
+    private double preco = 199.0;
+    private double desconto = 0;
 }
--- src/Service.java
+++ src/Service.java
@@ -1,2 +1,5 @@
 public class Service {
+    private String tipo;
+    private double precoPorHora = 100;
+    private double desconto = 10;
 }
