431b029f9b74c89193c84eb2c6add97cdd5050e4
Hiago 7k <hiago7k@gmail.com>
2026-07-20 15:56:22 -0300

    interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 class Main{
     static void main() {
 
-    BankAccount contaHiago = new BankAccount();
-    System.out.println(contaHiago.consultarSaldo());
-    contaHiago.Sacar(100);
-    contaHiago.Depositar(1000);
-
-    System.out.println("Saldo apos todas as operacoes "+ contaHiago.consultarSaldo());
-
-    System.out.println();
-    ContaCorrente tarifas = new ContaCorrente();
-    tarifas.cobrarTarifaMensal();
 
     }
 }
