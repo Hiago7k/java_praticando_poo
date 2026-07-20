f3dfc6d965e72614e939ba3eee2850d62d7b6d0a
Hiago 7k <hiago7k@gmail.com>
2026-07-20 16:27:56 -0300

    estanciando

--- src/ConversorTemparatura.java
+++ src/ConversorTemparatura.java
@@ -2,10 +2,12 @@ public class ConversorTemparatura implements ConvertorTemps {
     @Override
     public void celsiusParaFahrenheit(double tempC) {
     double fahrenheit = (tempC * 2) + 30;
+    System.out.println("Temperatura de Graus Celsius para Fahrenheit é " + fahrenheit);
     }
 
     @Override
     public void fahrenheitParaCelsius(double tempF) {
         double celsius =  (tempF - 32) / 2;
+        System.out.println("Temperatura de Fahrenheit para Graus Celsius é " + celsius);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 class Main{
     static void main() {
+    ConversorTemparatura termometro = new ConversorTemparatura();
+    termometro.celsiusParaFahrenheit(10);
+    termometro.fahrenheitParaCelsius(10);
 
     }
 }
