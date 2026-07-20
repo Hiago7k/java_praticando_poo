f8132605a0de4a6f3a274d6af6f7c0784a1154f0
Hiago 7k <hiago7k@gmail.com>
2026-07-20 16:40:41 -0300

    product service

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 class Main{
     static void main() {
-    ProdutoFisico mouse = new ProdutoFisico();
-    System.out.println("Preço final é " + mouse.calcularPrecoFinal());
-    // saída esperada de 120
 
-    Livro diarioDeUmBanana = new Livro();
-    System.out.println("Preço final é " + diarioDeUmBanana.calcularPrecoFinal());
-    // saída esperada de 90
     }
 }
 
