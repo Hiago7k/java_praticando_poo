fbb6418f289f3ae2ef97d70d4d6c6c727c46ccd3
Hiago 7k <hiago7k@gmail.com>
2026-07-21 16:12:14 -0300

    adicionando pessoas na lista

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+
 class Main{
     static void main() {
         Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa2 = new Pessoa();
+        Pessoa pessoa3 = new Pessoa();
         pessoa1.PessoasInfo("Hiago",21);
-        System.out.println(pessoa1.toString());
+        pessoa2.PessoasInfo("Ricardo",22);
+        pessoa3.PessoasInfo("Jose",10);
+
+        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+        listaDePessoas.add(pessoa1);
+        listaDePessoas.add(pessoa2);
+        listaDePessoas.add(pessoa3);
+        System.out.println(listaDePessoas);
     }
 }
 
