414d75e4b298eb7a8a5e0669eb89f1882086e4e3
Hiago 7k <hiago7k@gmail.com>
2026-07-21 16:02:37 -0300

    classe pessoa modificando o metodo tostring

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 class Main{
     static void main() {
-    Product mouses = new Product();
-    mouses.calcularPrecoFinal(); // 4,577
-
-        Service pedreiro = new Service();
-        pedreiro.calcularPrecoFinal(); // 490
 
     }
 }
